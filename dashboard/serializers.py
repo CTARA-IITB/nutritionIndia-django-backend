459adc33968d4373264f7349380cadea18a239da
AnimeshN <animesh.nauti@gmail.com>
2020-10-23 12:13:25 +0530

    areaendropdown renaming fields

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -7,9 +7,21 @@ class AreaEnSerializer(serializers.ModelSerializer):
         fields = ('area_id', 'area_code', 'area_name')
 
 class AreaEnDropSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AreaEn
-        fields = ('area_id', 'area_name')
+   # area_parent_id= serializers.DecimalField(max_digits=255, decimal_places=3)	
+    # area_level= serializers.DecimalField(max_digits=255, decimal_places=3)	
+    value = serializers.SerializerMethodField('get_area_id')	# renaming the field from area_id to value
+    label = serializers.SerializerMethodField('get_area_name')	#renaming
+
+
+    class Meta:	
+        model = AreaEn	
+        fields = ('value','label')	
+
+    def get_area_id(self, obj):	
+        return obj.area_id	
+
+    def get_area_name(self, obj):	
+        return obj.area_name 
 
 class IndicatorSerializer(serializers.ModelSerializer):
     class Meta:
